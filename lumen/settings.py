9138334e46918cb4147db2f0e288e0e037f0586a
Enzslv4 <enzosilva1420002@gmail.com>
2026-04-22 23:00:55 -0300

    Ajuste de allowed host sem o https no inicio da string

--- lumen/settings.py
+++ lumen/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ["https://lumem-gerenciador-de-biblioteca.onrender.com"]
+ALLOWED_HOSTS = ["lumem-gerenciador-de-biblioteca.onrender.com"]
 
 
 # Application definition
