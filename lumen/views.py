284d72c2cea89a82a084b65b3e55e443e8f8b6ff
Enzslv4 <enzosilva1420002@gmail.com>
2026-04-25 22:42:06 -0300

    Adicionando rota temporaria para add superuser

--- lumen/views.py
+++ lumen/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from apps.accounts import models
 
 def home(request):
     return render(request, 'home.html')
+
+def create_super(request):
+    from models import User
+    User.objects.filter(username='admin_lumen').delete()
+    User.objects.create_superuser(
+        username='admin_lumen',
+        email='seu@email.com',
+        password='sua_nova_senha'
+    )
+    from django.http import HttpResponse
+    return HttpResponse('Superusuário criado com sucesso!')
